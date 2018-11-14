4ddc3d94105bbe15a10686526d5cd7f2c5e0a978
= <filip@filipkarlsson.com>
2018-11-13 23:18:15 -0600

    RRT path finding works

--- scripts/rrt.py
+++ scripts/rrt.py
@@ -29,11 +29,13 @@ class Node:
 class RRT:
 
     def __init__(self, start_x, start_y, goal_x, goal_y, pixels, map_w, map_h):
-        self.nodes = [Node(start_x, start_y, None)]
+        self.start = Node(start_x, start_y, None)
         self.goal = Node(goal_x, goal_y, None)
+        self.nodes = [self.start]
         self.map_w = map_w
         self.map_h = map_h
         self.pixels = pixels
+        self.path = []
 
     def reachable(self, x1, y1, x2, y2, w):
         dx = x2 - x1
@@ -119,17 +121,59 @@ class RRT:
         canvas.create_image((self.map_w / 2, self.map_h / 2),
                             image=img, state="normal")
 
+        # Draw map
         for i in range(len(self.pixels)):
             u = i % self.map_w
             v = i // self.map_w
             if not self.pixels[i]:
                 img.put("#000", (u, v))
 
+        # Draw RRT
         for node in self.nodes:
             if node.prev != None:
                 canvas.create_line(node.x, node.y, node.prev.x, node.prev.y)
 
+        # Draw path
+        for node in self.path:
+            if node.prev != None:
+                canvas.create_line(node.x, node.y, node.prev.x,
+                                   node.prev.y, width=4, fill="#f00")
+
+        dot_size = 5
+        # Mark goal position
+        canvas.create_oval(self.goal.x - dot_size, self.goal.y - dot_size, self.goal.x +
+                           dot_size, self.goal.y + dot_size, outline="#000", fill="#00f")
+        # Mark start position
+        canvas.create_oval(self.start.x - dot_size, self.start.y - dot_size, self.start.x +
+                           dot_size, self.start.y + dot_size, outline="#000", fill="#00f")
+
         mainloop()
+
+    def calculate_path(self, iterations):
+        nr = 0
+        path_found = False
+        self.path = []
+
+        for i in range(iterations):
+            if self.add_random_node():
+                if distance(self.nodes[-1].x, self.nodes[-1].y, self.goal.x, self.goal.y) < RADIUS:
+                    if self.reachable(self.nodes[-1].x, self.nodes[-1].y, self.goal.x, self.goal.y, w):
+                        self.goal.prev = self.nodes[-1]
+                        self.nodes.append(self.goal)
+                        path_found = True
+                        break
+                nr += 1
+
+        if path_found:
+            self.path.append(self.goal)
+            current = self.goal
+            while current.prev != None:
+                self.path.insert(0, current.prev)
+                current = current.prev
+
+            return self.path
+
+        return None
 
 
 def distance(x1, y1, x2, y2):
@@ -142,18 +186,8 @@ if __name__ == '__main__':
         filename=world_map)
     w, h, pixels, metadata = r.read_flat()
 
-    rrt = RRT(100, 200, 400, 400, pixels, w, h)
+    rrt = RRT(100, 200, 600, 500, pixels, w, h)
 
-    nr = 0
-
-    # print(rrt.reachable(180, 300, 207, 450, ROBOT_SIZE))
-
-    for i in range(10000):
-        if rrt.add_random_node():
-            if distance(rrt.nodes[-1].x, rrt.nodes[-1].y, rrt.goal.x, rrt.goal.y) < RADIUS:
-                if rrt.reachable(rrt.nodes[-1].x, rrt.nodes[-1].y, rrt.goal.x, rrt.goal.y, w):
-                    break
-            nr += 1
-    print nr
+    rrt.calculate_path(20000)
 
     rrt.draw()
